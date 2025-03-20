cbfc19a5796027e7b0d867478274fe9989b495e0
TRJoseph <steppingbeast@gmail.com>
2025-03-20 19:24:10 -0400

    created train and test loop, nn module first version

--- training.py
+++ training.py
@@ -20,7 +20,12 @@ class ChessDataset(Dataset):
         # mask for boolean array of every evaluation with checkmate in x number of moves
         mask = self.chess_labels["eval"].str.contains("#", na=False)
 
-
+        # this sets the matescore and adds a bias depending on how many moves away from mate it is
+        # for example, "#+4" (mate-in-4 for white) = 10000 + (100/4) = 10025 whereas "#+1" (mate-in-1 for white) = 10000 + (100/1) = 10100
+        # this will hopefully reflect to the NN that a quicker mate is a "better" position
+        self.chess_labels.loc[mask, "eval"] = self.chess_labels.loc[mask, "eval"].apply(
+            lambda x: 10000 + (100/int(x.replace("#", ""))) if int(x.replace("#", "")) > 0 else -10000 + (100/int(x.replace("#", "")))
+        )
 
         self.transform = transform
         self.target_transform = target_transform
@@ -44,10 +49,68 @@ class ChessDataset(Dataset):
         tensor = torch.tensor(features, dtype=torch.float32), torch.tensor([float(evaluation)], dtype=torch.float32)
         return tensor
 
+class CReLU(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return torch.cat((self.relu(x), self.relu(-x)), dim=1)
 class ChessNNUE(nn.Module):
     def __init__(self):
-        pass
+        super().__init__()
 
+        self.fc1 = nn.Linear(768, 1024) 
+        self.fc2 = nn.Linear(2048, 1)
+
+        # self.linear_relu_stack = nn.Sequential(
+        #     nn.Linear(768, 1024),
+        #     nn.Linear(2048, 1)
+        # )
+    def forward(self, x):
+        hidden = self.fc1(x)
+        combined = torch.cat((hidden, hidden), dim=1)
+        return self.fc2(combined)
+    
+def train_loop(dataloader, model, loss_fn, optimizer, batch_size):
+    size = len(dataloader.dataset)
+    # Set the model to training mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader, model, loss_fn):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
 def readData():
     with open("./Data/chessData.csv", "r") as f:
         next(f)
@@ -70,6 +133,13 @@ def main():
     #fen = readData()
     #createFeatureVector(chess.Board(fen))
 
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    print(f"Using {device} device")
+    model = ChessNNUE().to(device)
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    print(model)
+
     dataset = ChessDataset("chessData.csv")
     df = dataset.__getdataframe__()
     print(df.shape)
@@ -88,6 +158,13 @@ def main():
     for features, evals in test_dataloader:
         print("Test batch:", features.shape, evals.shape)
         break
+
+    # run train and test loops
+    train_loop(train_dataloader, model, loss_fn, optimizer, 64)
+    print("\n\n TRAIN LOOP COMPLETE \n\n")
+    test_loop(test_dataloader, model, loss_fn)
+    print("\n\n TEST LOOP COMPLETE \n\n")
+
 
 if __name__ == "__main__":
     main()
