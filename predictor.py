137780fc667b858d84027791e991c0ca87d655ab
Thomas Joseph <thomas.joseph@raamp.com>
2025-03-27 17:05:20 -0400

    some test prediction fens added to prediction file

--- predictor.py
+++ predictor.py
@@ -4,18 +4,29 @@ import chess
 import numpy as np
 
 
+# activation range scaling factor
+S_A = 127
+# weight scaling factor
+S_W = 64
+# output scaling factor
+S_O = 10000
+
 model = training.ChessNNUE()
 weights = torch.load("nnue_weights.pt") 
 state_dict = {
     "ft.weight": weights["ft.weight"] / 255,
-    "l1.weight": weights["l1.weight"] / 255,
+    "l1.weight": weights["l1.weight"] /255,
     "l2.weight": weights["l2.weight"] / 255,
     "l3.weight": weights["l3.weight"] / 255,
-    "ft.bias": weights["ft.bias"] / 255,   
+
+    "ft.bias": weights["ft.bias"] / 255,
     "l1.bias": weights["l1.bias"] / 255,
     "l2.bias": weights["l2.bias"] / 255,
     "l3.bias": weights["l3.bias"] / 255
 }
+
+
+model.load_state_dict(state_dict)
 model.load_state_dict(state_dict)
 model.eval()
 print("✅ Model loaded successfully!")
@@ -47,12 +58,17 @@ def fen_to_tensor(fen):
     # white_feature, black_feature, stm
     return torch.tensor(white_feature).unsqueeze(0), torch.tensor(black_feature).unsqueeze(0), torch.tensor(stm).unsqueeze(0)
 
-# Test FEN Position
-fen = "r1bqkb1r/pp2p1pp/5p1n/3P4/3N4/2N5/PPP1BPPP/R1BQK2R w KQkq - 0 1"
-white_feature, black_feature, stm = fen_to_tensor(fen)
+# Test FEN Positions (~equal position, heavily winning for white, heaviley winning for white (slightly less), another ~equal positions)
+fenList = ["1nr3k1/p4p2/1p4p1/2qP1p1p/8/1QP2P2/P2N2PP/4RK2 w - - 0 31", "8/3kP3/1K4p1/8/2P2p1p/8/6PP/8 w - - 0 67", "8/3kP3/1p4p1/1K5p/2P2p2/8/6PP/8 w - - 1 66", "3r2r1/p2k1p2/1p1pp2p/8/3P4/2P5/P3KPPP/R6R w - - 2 21", "5r1k/p1pq2p1/1p1p3p/3B1b1Q/2P5/2P1P2n/PP5P/3R2RK b - - 0 27"]
+evaluations = ["0", "+5933", "+2440", "0", "-1019"]
 
-# Make Prediction
-with torch.no_grad():
-    output = model(white_feature, black_feature, stm).item()
 
-print(f"♟️ Chess NNUE Evaluation: {output:.3f}")
+for i in range(0, len(fenList)):
+    white_feature, black_feature, stm = fen_to_tensor(fenList[i])
+    # Make Prediction
+    with torch.no_grad():
+        output = model(white_feature, black_feature, stm).item()  
+
+    print(f"Position {i+1} with Stockfish evaluation: {evaluations[i]}\n")
+    print(f"♟️ Chess NNUE Evaluation: {output:.3f}\n")
+
